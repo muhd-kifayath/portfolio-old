c425f786b03e316d5059b3bd0cef40f7ae9c0de4
muhd-kifayath <imkifayath@gmail.com>
2023-01-22 21:43:47 +0530

    Animations added to home-page

---
+++ src/components/Home/index.js
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Web Developer", "App Developer" ];
+  const period = 1500;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+
+
+    const timeout = setTimeout(() => {
+        setLetterClass('text-animate-hover')
+      }, 4000)
+
+    return () => [clearInterval(ticker), clearTimeout(timeout)]
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }  
+
+
+  const nameArray = [' ', 'K', 'i', 'f', 'a', 'y', 'a', 't', 'h']
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+          </h1>
+          <p>I'm a <span className='wrap'>{text}</span></p>
+          <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+      </div>
+
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Home
